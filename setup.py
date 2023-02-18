328c1bee7537f62f895f6b9e01b18c511608b2a7
xzong0619 <xzong@udel.edu>
2023-02-18 18:39:48 -0500

    Update software version

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open('README.rst', 'r') as fh:
 
 setuptools_info = {
 	'name': 'DescMap',
-	'version': '1.0.0',
+	'version': '1.0.1',
 	'author': 'Vlachos Research Group',
 	'author_email': 'vlachos@udel.edu',
 	'description': 'Descriptor Map (DescMap)',
